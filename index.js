33fa29431908e66a4726db73484bd9e0207b4795
Jasong237 <jasonguo4635@gmail.com>
2024-10-02 12:56:25 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const DOMSelectors = {
   header: document.querySelector("h1"),
-  item: document.querySelectorAll("li"),
   cardHeader: document.querySelector(".card-header"),
+  cardImg: document.querySelector(".card-img"),
+  cardAuthor: document.querySelector(".card-author"),
   button: document.querySelector(".btn"),
   form: document.querySelector("form"),
 };
-let student = {
-  name: "Ryan",
-  age: 15,
-  girlfriend: true,
-};
-console.log(DOMSelectors.item);
-console.log(student.name);
 
-DOMSelectors.button.addEventListener("click", function (event) {
+DOMSelectors.form.addEventListener("Submit", function (event) {
+  event.preventDefault();
+  event.target.cardHeader = "input";
+});
+
+/* DOMSelectors.button.addEventListener("click", function (event) {
   console.log(event.target.parentElement);
   event.target.style.backgroundColor = "red";
-});
-
-DOMSelectors.form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  console.log(document.querySelector("input").value);
-});
+}); */
